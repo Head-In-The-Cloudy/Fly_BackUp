e3675a96dd19ffb150604425e824a2caf27ced43
Adrian-Pierre <Adrian-Pierre@users.noreply.github.com>
2025-07-28 22:15:21 +0800

    1

---
+++ 2（成功遍历航点）/整机到手飞使用代码/CarryPilot_For_LaunchPad_20241010_Released/Output/quaternion.d
@@ -0,0 +1,4 @@
+.\output\quaternion.o: WP_Src\Quaternion.c
+.\output\quaternion.o: WP_Src\Quaternion.h
+.\output\quaternion.o: WP_Src\WP_Math.h
+.\output\quaternion.o: E:\Embedded_System\Environment\Keil\ARM\ARMCC\Bin\..\include\stdint.h
